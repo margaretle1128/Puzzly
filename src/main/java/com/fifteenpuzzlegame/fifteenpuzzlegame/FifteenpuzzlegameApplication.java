2c13ae4a73df3cd7a0b775d434d196f18c884b54
Margaret Le <margaretle1128@gmail.com>
2023-12-19 03:52:12 -0800

    Initial commit

---
+++ src/main/java/com/fifteenpuzzlegame/fifteenpuzzlegame/FifteenpuzzlegameApplication.java
@@ -0,0 +1,13 @@
+package com.fifteenpuzzlegame.fifteenpuzzlegame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FifteenpuzzlegameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FifteenpuzzlegameApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fifteenpuzzlegame/fifteenpuzzlegame/FifteenpuzzlegameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fifteenpuzzlegame.fifteenpuzzlegame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FifteenpuzzlegameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
